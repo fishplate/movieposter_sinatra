b8742a515cd30c7ef12e01fbf3710a389b0e5571
otaiga <karl.turner5@gmail.com>
2013-02-07 17:18:30 +0000

    included active record and pagination, date formatting

--- Gemfile
+++ Gemfile
@@ -4,3 +4,9 @@ gem 'sinatra'
 gem 'shotgun'
 gem 'httparty'
 
+# Adding in pagination
+gem 'will_paginate', '~> 3.0'
+gem 'will_paginate-bootstrap'
+
+# easier than messing with date stuff
+gem 'activesupport'
--- movie_poster.rb
+++ movie_poster.rb
@@ -1,5 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'will_paginate/array'
+require "will_paginate-bootstrap"
+
+#Adding this as I'm being a bit lazy
+require 'active_support'
+
 
 #Load application libraries
 Dir[File.join(File.dirname(__FILE__), "./lib/**/*.rb")].each do |file|
@@ -8,14 +14,17 @@ end
 
 include MovieFinder
 include AlbumFinder
+include ActiveSupport::Inflector
 
 helpers do
+
   def movie(query)
     if !query.empty?
       conf = image_config_settings
       results = movie_search(query)
       @results = removed_nil_images(results).paginate(:page => params[:page], :per_page => 1)
       @image = conf["base_url"] + conf["size"]
+      erb :results
     else
       redirect "/"
     end
@@ -25,17 +34,21 @@ helpers do
     if !query.empty?
       results = album_search(params[:search])
       @results = (results).paginate(:page => params[:page], :per_page => 1)
-      render "album/album_results"
+      erb :album_results
     else
       redirect "/"
     end
   end
 
-
   # delete a nil image record:
   def removed_nil_images(results)
     results.delete_if {|x| x["image"] == nil}
   end
+
+  def date_format(date)
+    Date.parse(date).strftime("#{ordinalize(Date.parse(date).day)} %B %Y")
+  end
+
 end
 
 
