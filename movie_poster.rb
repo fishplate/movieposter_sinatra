7f93920787ad8cedcf79ee87e5dc02a3a2266151
otaiga <karl.turner5@gmail.com>
2013-02-07 12:27:12 +0000

    added movie lib and required in main app

--- movie_poster.rb
+++ movie_poster.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'sinatra'
 
+#Load application libraries
+Dir[File.join(File.dirname(__FILE__), "../lib/**/*.rb")].each do |file|
+  require file
+end
+
 get "/" do
   erb :index
 end
